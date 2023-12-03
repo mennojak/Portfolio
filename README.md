36a2a14065dab043a1a5d30b1c7e082867a21823
Menno Jak <mennojak@gmail.com>
2023-12-03 12:29:07 +0100

    Full overhaul

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Portfolio
 
-My personal portfolio which shows my abilities and projects. 
+My portfolio website, which shows my abilities and projects. 
 
 To visit this site you can go to:
 https://mennojak.github.io/Portfolio/
--- assets/js/main.js
+++ assets/js/main.js
@@ -127,7 +127,7 @@
 
 		// Toggle.
 			$navPanelToggle = $(
-				'<a href="#navPanel" id="navPanelToggle"></a>'
+				'<a href="#navPanel" id="navPanelToggle">Menu</a>'
 			)
 				.appendTo($wrapper);
 
@@ -257,7 +257,31 @@
 
 })(jQuery);
 
-// For the collapsible description
-$('.btn').click(function() {
-	$(this).next().slideToggle(400);
-  });
+
+// Custom slideshows
+let slideIndex = [1,1];
+
+let slideId = ["mySlides1", "mySlides2"]
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
+	showSlides(slideIndex[no] += n, no);
+	}
+
+	function showSlides(n, no) {
+	let i;
+	let x = document.getElementsByClassName(slideId[no]);
+	if (n > x.length) {slideIndex[no] = 1}
+	if (n < 1) {slideIndex[no] = x.length}
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}
+	x[slideIndex[no]-1].style.display = "block";
+}
+
+// Custom popup
+function myFunction() {
+	var popup = document.getElementById("myPopup");
+	popup.classList.toggle("show");
+  }
