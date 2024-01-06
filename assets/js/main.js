e9e858bf4f3a58d918c04e6bc6b2f3e8f4b0f5bb
Menno Jak <mennojak@gmail.com>
2024-01-06 12:45:27 +0100

    Credit modal done

--- assets/js/main.js
+++ assets/js/main.js
@@ -285,3 +285,25 @@ function myFunction() {
 	var popup = document.getElementById("myPopup");
 	popup.classList.toggle("show");
   }
+
+
+// Custom modal
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+modal.style.display = "block";
+}
+
+span.onclick = function() {
+modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+if (event.target == modal) {
+modal.style.display = "none";
+}
+}
